410444b22f22fa25d8013b0ed005986daf397e0a
LuisaCampanhaH <campanhaluisa@gmail.com>
2026-02-09 18:22:58 -0300

    ADD: BeeCrowd (+1)

--- BeeCrowd/beginner/c/Consumption.c
+++ BeeCrowd/beginner/c/Consumption.c
@@ -1,16 +1,26 @@
-// PROBLEMA 1014 - Consumption
-// LINK - (https://judge.beecrowd.com/en/problems/view/1014)//
-// DESCRIÇÃO: consumo de gasolina de um carro(km por litro)
-// INPUT: X (KM) E Y(fuel)  //
-// OUTPUT : Z km/L//
-// LINGUAGEM: C //
-int main()
-{
-    double fuel;
+/*
+ * PROBLEMA: 1014 - Consumption (Consumo)
+ * PLATAFORMA: Beecrowd
+ * LINK: https://judge.beecrowd.com/en/problems/view/1014
+ * LINGUAGEM: C
+ *
+ * DESCRIÇÃO:
+ * Calcular o consumo médio de um automóvel (Km/L) com base na
+ * distância percorrida (X) e no combustível gasto (Y).
+ */
+
+#include <stdio.h>
+
+int main() {
     int km;
+    double fuel;
     double consumption;
-    scanf("%i %lf",&km,&fuel);
+
+    scanf("%d %lf", &km, &fuel);
+
     consumption = km / fuel;
-    printf("%.3lf km/l\n",consumption);
+
+    printf("%.3lf km/l\n", consumption);
+
     return 0;
 }
