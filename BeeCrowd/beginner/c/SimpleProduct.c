340e71f35b8375d15132e09a97f552df1f4b0e0f
LuisaCampanhaH <campanhaluisa@gmail.com>
2026-02-09 18:23:14 -0300

    ADD: BeeCrowd (+1)

--- BeeCrowd/beginner/c/SimpleProduct.c
+++ BeeCrowd/beginner/c/SimpleProduct.c
@@ -1,15 +1,25 @@
-// PROBLEMA 1004 - Simple Product//
-// LINK - (https://judge.beecrowd.com/en/problems/view/1004)//
-// DESCRIÇAO: UMA MULTIPLIÇÃO SIMPLES DE DOIS NÚMEROS NATURAIS//
-// INPUT: 30 20 //
-// OUTPUT : SOMA: 60 //
-// LINGUAGEM: C //
+/*
+ * PROBLEMA: 1004 - Simple Product (Produto Simples)
+ * PLATAFORMA: Beecrowd
+ * LINK: https://judge.beecrowd.com/en/problems/view/1004
+ * LINGUAGEM: C
+ *
+ * DESCRIÇÃO:
+ * Ler dois valores inteiros, calcular o produto entre eles e 
+ * mostrar o resultado conforme o padrão exigido.
+ */
 
-int main()
-{
+#include <stdio.h>
+
+int main() {
     int a, b, produto;
-    scanf("%i %i",&a,&b);
+
+    scanf("%d %d", &a, &b);
+
     produto = a * b;
-    printf("%i",produto);
+
+    // Atenção: O Beecrowd exige espaços antes e depois do '=' e o \n no final
+    printf("PROD = %d\n", produto);
+
     return 0;
 }
