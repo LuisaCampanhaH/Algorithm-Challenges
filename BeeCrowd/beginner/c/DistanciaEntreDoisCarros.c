9e7ed94fbb110a0df85a4e17cdf3ee49e625e6d3
LuisaCampanhaH <campanhaluisa@gmail.com>
2026-02-11 15:46:04 -0300

    ADD: BeeCrowd (+1)

---
+++ BeeCrowd/beginner/c/DistanciaEntreDoisCarros.c
@@ -0,0 +1,14 @@
+// Exercicio 1016 Distance
+// Link:https://judge.beecrowd.com/en/problems/view/1016
+// Descrição: Quantos minutos demora para o carra y se afastar X kilometros do carro a, sendo a velocidade de y 90km/h e a velocidade de a 60km/h
+
+#include <stdio.h>
+ 
+int main() {
+    int x,resposta;
+    scanf("%i",&x);
+    resposta = x*2;
+    printf("%i minutos\n",resposta);
+ 
+    return 0;
+}
