ec3f09398359a2d600323a6ea7788fbace04d0fb
LuisaCampanhaH <campanhaluisa@gmail.com>
2026-02-11 14:42:30 -0300

    ADD: BeeCrowd (+1)

---
+++ BeeCrowd/beginner/c/SimpleProduct.c
@@ -0,0 +1,13 @@
+// Exercicio 1004: Simple Product
+// Link: https://judge.beecrowd.com/en/problems/view/1004
+// Descrição: Multiplição simples de números inteiros
+
+#include <stdio.h>
+int main() {
+    int x,y;
+    int prod;
+    scanf("%i %i", &x,&y);
+    prod = x * y;
+    printf("PROD = %i\n",prod);
+    return 0;
+}
