74f32f933edb340bd55020d6d1c355d01e1b2289
LuisaCampanhaH <campanhaluisa@gmail.com>
2026-02-11 14:36:00 -0300

    ADD: BeeCrowd (+1)

---
+++ BeeCrowd/beginner/c/SimpleSum.c
@@ -0,0 +1,12 @@
+// Exercicio 1003: SIMPLE SUM
+// Link: https://judge.beecrowd.com/en/problems/view/1003
+// Descrição: Soma de dois inteiro
+#include <stdio.h>
+int main() {
+    int x; int y;
+    int soma; 
+    scanf("%i %i",&x,&y);
+    soma = x+y;
+    printf("SOMA = %i\n",soma);
+    return 0;
+}
